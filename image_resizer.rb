d2c55f693d9dac4c6aacd8d071f4e7fa650193ad
Patrick Wiseman <me@thephw.com>
2014-03-03 18:48:48 -0500

    Change image resizer to use net/http

--- image_resizer.rb
+++ image_resizer.rb
@@ -1,15 +1,22 @@
 require 'sinatra'
 require 'RMagick'
 require 'net/http'
+require 'uri'
 
 get '/*x*' do |width, height|
-  puts width, height, params[:url]
+  uri = URI.parse(params[:url])
   random_number = rand
   Dir.mkdir "tmp" unless Dir.exists? "tmp"
   output_doc = "tmp/file#{random_number}"
   output_thumb = "tmp/thumb#{random_number}"
-  puts "wget --output-document=#{output_doc} #{params[:url]}"
-  `wget --output-document=#{output_doc} #{params[:url]}`
+  # `wget --output-document=#{output_doc} #{params[:url]}`
+  # Must be somedomain.net instead of somedomain.net/, otherwise, it will throw exception.
+  Net::HTTP.start(uri.host) do |http|
+    resp = http.get(uri.path)
+    open(output_doc, "wb") do |file|
+      file.write(resp.body)
+    end
+  end
   img = Magick::Image::read(output_doc).first
   thumb = img.resize_to_fit(width, height)
   thumb.write output_thumb
